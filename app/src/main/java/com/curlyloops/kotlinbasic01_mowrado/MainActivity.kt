a95c325fdda90548c0d694337c0f2064ba963c02
KHS <hyoseo118@gmail.com>
2023-03-07 22:31:21 +0900

    토스트 띄워보기

--- app/src/main/java/com/curlyloops/kotlinbasic01_mowrado/MainActivity.kt
+++ app/src/main/java/com/curlyloops/kotlinbasic01_mowrado/MainActivity.kt
@@ -3,6 +3,7 @@ package com.curlyloops.kotlinbasic01_mowrado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,10 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면로그", "클릭용 버튼 눌림")
             Log.e("메인화면로그","에러 관련 로그")
 
+        }
 
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
         }
 
     }
