4df7695bfece752dc73c5eb0173d9cfcf637fc21
KHS <hyoseo118@gmail.com>
2023-03-07 22:23:56 +0900

    로그 찍어보기

--- app/src/main/java/com/curlyloops/kotlinbasic01_mowrado/MainActivity.kt
+++ app/src/main/java/com/curlyloops/kotlinbasic01_mowrado/MainActivity.kt
@@ -2,6 +2,7 @@ package com.curlyloops.kotlinbasic01_mowrado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
 
+            //코드에 메모 남기기 - 주석
+//            Ctrl + / : 커서가 있는줄을 주석 처리.
+//            이 {} 안에 있는 코드만, 버튼이 눌렸을때 실행됨
+
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+
+        }
 
     }
 
